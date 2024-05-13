c96fb861bf71899c518357232f631ba74dfff5e1
Deanna Stevens <deanna.sofias@gmail.com>
2024-05-13 00:01:49 -0400

    Add psuedocode

---
+++ src/botSavesPrincess2.js
@@ -0,0 +1,27 @@
+/*
+
+Princess Peach and bot's position are randomly set
+Four parameters in nextMove
+Sample grid is 5x5
+Print out only ONE move at a time, the next move
+
+INPUT: 4 params: integer N, integer r (bot's row position), integer c (bot's column position), and char array grid
+  Sample:
+  5
+  2 3
+  -----
+  -----
+  p--m-
+  -----
+  -----
+
+OUTPUT: print the next move (LEFT, RIGHT, UP or DOWN)
+
+GOAL:
+  reach the princess in as few moves as possible and print to the console only the next step
+
+HOW:
+  find bot and princess
+  determine direction
+  output moves by printing to console
+*/
